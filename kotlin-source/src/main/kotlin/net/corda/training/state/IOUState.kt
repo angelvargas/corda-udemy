c60c248131eea256d656f82759ce79a355b9b529
Angel Florentino Vargas Pool <angelvargaspool@gmail.com>
2019-09-25 17:38:35 -0500

    States

--- kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
@@ -1,9 +1,11 @@
 package net.corda.training.state
 
-import net.corda.core.contracts.BelongsToContract
-import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.*
 import net.corda.core.identity.Party
+import net.corda.finance.POUNDS
 import net.corda.training.contract.IOUContract
+import java.util.*
+import javax.servlet.http.Part
 
 /**
  * This is where you'll add the definition of your state object. Look at the unit tests in [IOUStateTests] for
@@ -12,6 +14,14 @@ import net.corda.training.contract.IOUContract
  * Remove the "val data: String = "data" property before starting the [IOUState] tasks.
  */
 @BelongsToContract(IOUContract::class)
-data class IOUState(val data: String = "data"): ContractState {
-    override val participants: List<Party> get() = listOf()
+data class IOUState(
+        val amount: Amount<Currency>,
+        val lender: Party,
+        val borrower: Party,
+        val paid: Amount<Currency> = Amount(0, amount.token),
+        override val linearId: UniqueIdentifier = UniqueIdentifier()
+): LinearState {
+    fun pay(AmountToPay:Amount<Currency>) = copy(paid = paid.plus(AmountToPay))
+    fun withNewLender(NewLender: Party) = copy(lender = NewLender)
+    override val participants: List<Party> get() = listOf(lender,borrower)
 }
